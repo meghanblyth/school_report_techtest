c9f4b6e1756912bd3a318a87b65ff01cc35536b3
meghanblyth <megblyth@gmail.com>
2022-05-10 22:01:34 +0100

    Passing four basic tets before refactoring

--- lib/report.rb
+++ lib/report.rb
@@ -1,4 +1,13 @@
 
 def report(string)
-  return "Green: 1" 
+  if string == "Green"
+    return "Green: 1" 
+  elsif string == "Red"
+    return "Red: 1"
+  elsif string == "Amber"
+    return "Amber: 1"
+  elsif string == ""
+    return "No results given"
+  end 
 end 
+
--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -2,6 +2,19 @@ require 'report'
 
 describe 'report' do 
   it 'should receive a string as an argument' do 
-    expect(report("Green: 1")).to eq "Green: 1"
+    expect(report("Green")).to eq "Green: 1"
   end 
+
+  it 'should receive a string as an argument' do 
+    expect(report("Red")).to eq "Red: 1"
+  end
+
+  it 'should receive a string as an argument' do 
+    expect(report("Amber")).to eq "Amber: 1"
+  end
+
+  it 'should read an empty string and return a message' do 
+    expect(report("")).to eq "No results given"
+  end 
+
 end 
